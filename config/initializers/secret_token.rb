a6cde42e610d98295bdb916e800c04e0543bcb9c
seit <uent192837465@gmail.com>
2012-03-28 23:44:20 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tbrsample::Application.config.secret_token = '8f7144486196ace4119dda6702a772372de9e4977e00e9682575b8e1a28551529ec3c82aea51b69fb15e56f133fcc9977c3760f4395b4a93ecc45c87382f5561'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tbrsample::Application.config.session_store :cookie_store, :key => '_tbrsample_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tbrsample::Application.config.session_store :active_record_store
